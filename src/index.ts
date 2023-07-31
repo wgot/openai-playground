f5f8a81b5ed0eff091c544381e6c30e8b9c42155
Hiroshi Tanaka <tanaka.hiroshi@bizforward.co.jp>
2023-07-31 23:10:47 +0900

    Add transcription from audio file.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import { notStrictEqual } from 'assert'
+import { summarize } from './summarize'
 import Transcribe from './transcribe'
+import Transcript from './transcript'
 
 (async ([cmd, arg]) => {
   console.log(cmd, arg)
   switch (cmd) {
+    case 'transcript':
+      notStrictEqual(arg, undefined)
+      const transcript = await new Transcript(arg).start()
+      console.log(transcript)
+      const summary = await summarize(transcript)
+      console.log('summary:', summary)
+      break
     case 'transcribe':
+      notStrictEqual(arg, undefined)
       new Transcribe(arg, 'ja').start()
       break
     default:
